d3dfd273e6359ea70a2cc60f3d3824e147763c3c
Ralf Ebert <info@ralfebert.de>
2018-07-16 21:39:23 +0200

    Initial Commit

---
+++ HelloApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Ralf Ebert on 16.07.18.
+//  Copyright © 2018 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
